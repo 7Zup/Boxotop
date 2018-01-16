63e8a66273a10ff19c5c59c28268b70529132665
Fabrice Froehly <fabrice.froehly@epitech.eu>
2018-01-16 22:10:51 +0100

    Init of the project adding HomeView and HomeVC

---
+++ Boxotope/Boxotop/Controller/HomeVC.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  Boxotop
+//
+//  Created by Fabrice Froehly on 16/01/2018.
+//  Copyright © 2018 Fabrice Froehly. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Manage elements in the Home view
+class HomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+}
