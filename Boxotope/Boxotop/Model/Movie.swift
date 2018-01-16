6e3d1f000dbb01a0fdbcb14cf8eda3ca72f74495
Fabrice Froehly <fabrice.froehly@epitech.eu>
2018-01-16 22:09:23 +0100

    Creation of the models Movie and Movies

---
+++ Boxotope/Boxotop/Model/Movie.swift
@@ -0,0 +1,54 @@
+//
+//  Movie.swift
+//  Boxotop
+//
+//  Created by Fabrice Froehly on 16/01/2018.
+//  Copyright © 2018 Fabrice Froehly. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+
+/// Class Movies that contains a list of movies
+class Movies: Mappable {
+    
+    var movies: [Movie]?
+    var totalResults: Int?
+    
+    required init?(map: Map) {
+    }
+    
+    func mapping(map: Map) {
+        
+        movies          <- map["Search"]
+        totalResults    <- map["totalResults"]
+    }
+}
+
+
+
+
+
+/// Class Movie that contains the information about a movie
+class Movie: Mappable {
+    
+    var title: String?
+    var year: Int?
+    var imdbID: Int?
+    var type: String?
+    var poster: String?
+    
+    required init?(map: Map) {
+    }
+    
+    func mapping(map: Map) {
+        
+        title           <- map["Title"]
+        year            <- map["Year"]
+        imdbID          <- map["imdbID"]
+        type            <- map["Type"]
+        poster          <- map["Poster"]
+    }
+}
+
