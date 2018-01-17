09b5866be8d3280769107e2d5e08e4f5faa5af7f
Fabrice Froehly <fabrice.froehly@epitech.eu>
2018-01-17 14:23:20 +0100

    Begin of home view - elements added, still need a bit of configuration + api call

--- Boxotop/Boxotop/Controller/HomeVC.swift
+++ Boxotop/Boxotop/Controller/HomeVC.swift
@@ -9,14 +9,65 @@
 import Foundation
 import UIKit
 
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageLabel: UIImageView!
+}
+
 /// Manage elements in the Home view
 class HomeVC: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.isNavigationBarHidden = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+    }
+}
+
+extension HomeVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: MovieCell = tableView.dequeueReusableCell(withIdentifier: "movieCell") as! MovieCell
+        
+        cell.titleLabel.text = "movieTitle"
+        
+        // Background color of cell
+        if indexPath.row % 2 == 0 {
+            
+            cell.backgroundColor = Colors.customLightGreen
+        } else {
+            
+            cell.backgroundColor = UIColor.white
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 70
     }
 }
---
+++ Boxotop/Boxotop/Utils/Colords.swift
@@ -0,0 +1,15 @@
+//
+//  EXUIColor.swift
+//  Boxotop
+//
+//  Created by Fabrice Froehly on 17/01/2018.
+//  Copyright © 2018 Fabrice Froehly. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Colors {
+    
+    static let customLightGreen: UIColor = UIColor(red: 182, green: 215/255, blue: 168, alpha: 1)
+}
